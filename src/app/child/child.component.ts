5c86a66d6e641e5f7c148282117e3feca1680161
Abdelrahman-88 <abdo.elsaid@hotmail.com>
2021-11-09 10:27:26 +0200

    update

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
@@ -10,6 +11,22 @@ export class ChildComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  very(){
+    document.getElementById("very")?.scrollIntoView({behavior:"smooth"})
+  }
+  over(){
+    document.getElementById("over")?.scrollIntoView({behavior:"smooth"})
+  }
+  healthy(){
+    document.getElementById("healthy")?.scrollIntoView({behavior:"smooth"})
+  }
+  under(){
+    document.getElementById("under")?.scrollIntoView({behavior:"smooth"})
+  }
+  change(){
+    document.getElementById("change")?.scrollIntoView({behavior:"smooth"})
   }
 
 }
